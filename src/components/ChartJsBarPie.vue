326124bdc274c80f3c80dc7005a9f10480562543
nottnt <33511398+nottnt@users.noreply.github.com>
2024-10-03 11:38:59 +0700

    Feature/custom style (#2)
    * feat: add `sass-embedded`
    
    * chore: add font
    
    * chore: remove style.css
    
    * feat: add constant
    
    * feat: font and css configuration
    
    * chore: move constant file
    
    * chore: add utils
    
    * feat: add interact btn to echart
    
    * feat: add interact btn to chartJs

--- src/components/ChartJsBarPie.vue
+++ src/components/ChartJsBarPie.vue
@@ -2,18 +2,36 @@
   <div>
     <h2>Bar Chart (vue-chartjs)</h2>
     <div class="chart-container">
-      <Bar id="chart-js-bar" :data="barData" :options="chartOptions" />
+      <Bar id="chart-js-bar" :data="barData" :options="barChartOptions" />
     </div>
+    <v-btn
+      color="secondary"
+      variant="outlined"
+      @click="() => handleClickInteractChart(CHART_TYPE.BAR)"
+      >Interact Bar</v-btn
+    >
 
     <h2>Pie Chart (vue-chartjs)</h2>
     <div class="chart-container">
-      <Pie id="chart-js-pie" :data="pieData" :options="chartOptions" />
+      <Pie id="chart-js-pie" :data="pieData" :options="pieChartOptions" />
     </div>
+    <v-btn
+      color="secondary"
+      variant="outlined"
+      @click="() => handleClickInteractChart(CHART_TYPE.PIE)"
+      >Interact Pie</v-btn
+    >
 
     <h2>Line Chart (vue-chartjs)</h2>
     <div class="chart-container">
       <Line id="chart-js-line" :data="lineData" :options="lineChartOptions" />
     </div>
+    <v-btn
+      color="secondary"
+      variant="outlined"
+      @click="() => handleClickInteractChart(CHART_TYPE.LINE)"
+      >Interact Line</v-btn
+    >
   </div>
 </template>
 
@@ -37,6 +55,8 @@ import {
   TooltipItem,
   ChartTypeRegistry,
 } from 'chart.js'
+import { CHART_TYPE } from '../constant'
+import { randomNumber } from '../utils'
 
 // Register components needed for Line chart
 ChartJS.register(
@@ -54,7 +74,7 @@ ChartJS.register(
 // Get the current Vuetify theme
 const theme = useTheme()
 
-const barData = ref<ChartData<'bar'>>({
+const barData = ref<ChartData<CHART_TYPE.BAR>>({
   labels: ['January', 'February', 'March', 'April'],
   datasets: [
     {
@@ -70,7 +90,7 @@ const barData = ref<ChartData<'bar'>>({
   ],
 })
 
-const pieData = ref<ChartData<'pie'>>({
+const pieData = ref<ChartData<CHART_TYPE.PIE>>({
   labels: ['Red', 'Blue', 'Yellow'],
   datasets: [
     {
@@ -86,7 +106,7 @@ const pieData = ref<ChartData<'pie'>>({
 })
 
 // Line Chart Data
-const lineData = ref<ChartData<'line'>>({
+const lineData = ref<ChartData<CHART_TYPE.LINE>>({
   labels: ['January', 'February', 'March', 'April'],
   datasets: [
     {
@@ -100,7 +120,7 @@ const lineData = ref<ChartData<'line'>>({
   ],
 })
 
-const chartOptions = ref<ChartOptions<'pie' | 'bar'>>({
+const barChartOptions = ref<ChartOptions<CHART_TYPE.BAR>>({
   responsive: true,
   maintainAspectRatio: true,
   plugins: {
@@ -114,7 +134,7 @@ const chartOptions = ref<ChartOptions<'pie' | 'bar'>>({
   },
 })
 
-const lineChartOptions = ref<ChartOptions<'line'>>({
+const pieChartOptions = ref<ChartOptions<CHART_TYPE.PIE>>({
   responsive: true,
   maintainAspectRatio: true,
   plugins: {
@@ -127,6 +147,70 @@ const lineChartOptions = ref<ChartOptions<'line'>>({
     },
   },
 })
+
+const lineChartOptions = ref<ChartOptions<CHART_TYPE.LINE>>({
+  responsive: true,
+  maintainAspectRatio: true,
+  plugins: {
+    tooltip: {
+      backgroundColor: '#424242',
+      callbacks: {
+        label: function (tooltipItem: TooltipItem<keyof ChartTypeRegistry>) {
+          return `Custom Tooltip: ${tooltipItem.dataset.label} - ${tooltipItem.raw} units`
+        },
+      },
+    },
+  },
+})
+
+const handleClickInteractChart = (type: CHART_TYPE) => {
+  if (type === CHART_TYPE.BAR) {
+    barData.value = {
+      ...barData.value,
+      datasets: [
+        {
+          label: 'Sales',
+          backgroundColor: [
+            theme.current.value.colors.primary,
+            theme.current.value.colors.secondary,
+            theme.current.value.colors.success,
+            theme.current.value.colors.error,
+          ],
+          data: [randomNumber(), randomNumber(), randomNumber(), randomNumber()],
+        },
+      ],
+    }
+  } else if (type === CHART_TYPE.PIE) {
+    pieData.value = {
+      ...pieData.value,
+      datasets: [
+        {
+          label: 'Population',
+          backgroundColor: [
+            theme.current.value.colors.error,
+            theme.current.value.colors.blue,
+            theme.current.value.colors.warning,
+          ],
+          data: [randomNumber(), randomNumber(), randomNumber()],
+        },
+      ],
+    }
+  } else {
+    lineData.value = {
+      ...lineData.value,
+      datasets: [
+        {
+          label: 'Temperature',
+          borderColor: theme.current.value.colors.blue,
+          backgroundColor: theme.current.value.colors.blue,
+          data: [randomNumber(), randomNumber(), randomNumber(), randomNumber()],
+          fill: false,
+          tension: 0.1,
+        },
+      ],
+    }
+  }
+}
 </script>
 
 <style></style>
---
+++ src/utils.ts
@@ -0,0 +1,3 @@
+export const randomNumber = (max: number = 100) => {
+  return Math.floor(Math.random() * max)
+}
